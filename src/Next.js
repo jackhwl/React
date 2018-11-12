4dcf567503f433633fbf3fb502c7cba774ab1fcb
jackhwl <jackhwl@hotmail.com>
2018-11-11 21:38:11 -0500

    p22 list and keys

--- src/Next.js
+++ src/Next.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Next = () => 
         <h3>Next step: read more about: 
-             <a href="https://reactjs.org/docs/rendering-elements.html" target="_blank">
-                 rendering elements in React
+             <a href="https://reactjs.org/docs/lists-and-keys.html" target="_blank">
+             React lists and keys
              </a>
         </h3>;
 
