5bc6a54c3fa0603a27ba7b481d95612cd6edd83c
jackhwl <jackhwl@hotmail.com>
2018-11-11 19:26:57 -0500

    change next to arrow function

---
+++ src/Next.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Next = () => 
+        <h3>Next step: read more about: 
+             <a href="https://reactjs.org/docs/rendering-elements.html" target="_blank">
+                 rendering elements in React
+             </a>
+        </h3>;
+
+export default Next;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Next from './Next';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<h3>Next step: read more about <a href="https://reactjs.org/docs/rendering-elements.html" target="_blank">
-rendering elements in React</a></h3>, document.getElementById('next'));
+ReactDOM.render(<Next />, document.getElementById('next'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
