3afb958841db8234b49b4cf86d22c2270fdc7808
Wenlin Huang <jackhwl@hotmail.com>
2023-06-07 17:33:50 -0400

    Understanding React without a Build Chain or JSX Syntax

--- WhatIsReact/Readme.md
+++ WhatIsReact/Readme.md
@@ -1,2 +1,3 @@
 ## Section 3: React Architecture Explained
 * What It Means That React Is a Single Page App (SPA)
+* Understanding React without a Build Chain or JSX Syntax
--- WhatIsReact/m3/index.js
+++ WhatIsReact/m3/index.js
@@ -1,9 +1,20 @@
 window.onload = () => {
   const rootElement = document.getElementById("root");
-  const button = document.createElement("button");
-  button.innerHTML = "Click me for current date";
-  button.addEventListener("click", () => {
-    button.innerHTML = new Date().toString();
-  });
-  rootElement.appendChild(button);
+  const ints = [1, 2, 3];
+
+  // ints.forEach((i) => {
+  //   let li = document.createElement("li");
+  //   li.innerHTML = i;
+  //   rootElement.appendChild(li);
+  // });
+
+  const root = ReactDOM.createRoot(rootElement);
+
+  const childrenElements = [];
+
+  childrenElements.push(React.createElement("li", { key: ints[0] }, ints[0]));
+  childrenElements.push(React.createElement("li", { key: ints[1] }, ints[1]));
+  childrenElements.push(React.createElement("li", { key: ints[2] }, ints[2]));
+
+  root.render(childrenElements);
 };
