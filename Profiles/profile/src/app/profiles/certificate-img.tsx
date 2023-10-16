8f7018ae67229d97774eed4f5a1400d8bb8c34ab
Wenlin Huang <jackhwl@hotmail.com>
2023-10-16 09:02:29 -0400

    fix data path

--- Profiles/profile/src/app/profiles/certificate-img.tsx
+++ Profiles/profile/src/app/profiles/certificate-img.tsx
@@ -1,11 +1,20 @@
 import "server-only";
-import imgDataAll from "../../../data/imgdata.json";
+//import imgDataAll from "../../../data/imgdata.json";
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-export default function CertificateImg({ id }: { id: string }) {
+async function getSessionVideo(id: string) {
+  await delay(2000);
+  const res = await fetch(`http://localhost:3000/api/youtubedata/${id}`);
+  if (!res.ok) { throw new Error("Error fetching session video")}
+  const data = await res.json();
+  return data
+}
+
+export default async function CertificateImg({ id }: { id: string }) {
   if (!id || id.length === 0) {
     return null;
   }
-  const imgData = imgDataAll.data.youTubeData.find(rec => rec.id === id);
+  const imgData = await getSessionVideo(id)
   return (
     <div className="card">
       <a target="_blank" href={`https://www.youtube.com/watch?v=${id}`}>
