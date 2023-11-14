d21111e484c98d5e4d7b17f547647a64db82ba78
Wenlin Huang <jackhwl@hotmail.com>
2023-11-14 12:43:44 -0500

    Server Action Mutation

--- IntroToNextJs13/intro-v3-app/app/components/Todo.tsx
+++ IntroToNextJs13/intro-v3-app/app/components/Todo.tsx
@@ -1,5 +1,12 @@
+'use client'
+
+import { completeTodo } from "@/utils/actions"
+import { useTransition } from "react"
+
 const Todo = ({todo}) => {
-    return <div>{todo.content}</div>
+    const [ispending, startTransition] = useTransition()
+    return (<div className={`border boorder-black/20 cursor-pointer ${todo.completed ? 'line-through text-gray-900' : ''}`}
+    onClick={() => startTransition(() => completeTodo(todo.id))}>{todo.content}</div>)
 }
 
 export default Todo
